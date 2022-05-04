390e96c836544f3caac9f9cc859026d9751844d1
Nikita Reshetnik <reshetnik.nikita@gmail.com>
2022-05-04 12:03:04 +0300

    Added missing files

---
+++ src/app/pages/home/interfaces/educationModel.ts
@@ -0,0 +1,9 @@
+export interface IEducationModel {
+  educationalInstitution: string,
+  degree: string,
+  speciality: string,
+  startYear: string,
+  endYear: string,
+  description: string,
+}
+
---
+++ src/app/pages/home/interfaces/experienceModel.ts
@@ -0,0 +1,13 @@
+export interface IExperienceModel {
+  positionTitle: string,
+  company: string,
+  startMonth: string,
+  startYear: string,
+  endMonth: string
+  endYear: string,
+  description: {
+    responsibilities: string[],
+    aboutProject: string,
+    toolsAndTechnologies: string
+  },
+}
